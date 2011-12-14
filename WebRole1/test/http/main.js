1a0eacb9c98cfd9f84c1829354326840aa00ab92
Raynos <=>
2011-12-14 18:35:02 +0000

    Added HTTP request stub to the test / function

--- WebRole1/test/http/main.js
+++ WebRole1/test/http/main.js
@@ -2,6 +2,24 @@ var http = require("http");
 
 module.exports = {
 	"test /": function (test) {
-		test.done();		
+		test.expect(2);
+
+		var req = http.request({
+			host: "localhost",
+			port: 81,
+			path: "/"
+		}, handleResponse);
+		
+		req.on("error", throwError);
+
+		req.end();
+
+		function handleResponse() {
+			
+		}
 	}
 };
+
+function throwError(err) {
+	throw err;
+}
